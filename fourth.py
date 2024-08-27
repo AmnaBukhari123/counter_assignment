24264184769b9b86f05b2ecafbb7d6fdc5805135
AmnaBukhari123 <bukhariamna258@gmail.com>
2024-08-27 11:37:43 +0500

    Add fourth.py

---
+++ fourth.py
@@ -0,0 +1,11 @@
+import time, sys, keyboard
+
+target = int(sys.argv[1])
+
+for counter in range(1, target + 1):
+    if keyboard.is_pressed('space'):
+        print("Counter stopped by pressing space key.")
+        break
+
+    print(counter)
+    time.sleep(1)
