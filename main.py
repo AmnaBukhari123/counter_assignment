96a786069c3b17bea6968898ba45181bc785c438
AmnaBukhari123 <bukhariamna258@gmail.com>
2024-08-28 12:32:29 +0500

    Changes made to main.py again

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ import time
 import keyboard
 import argparse
 
-parser = argparse.ArgumentParser( prog="Commandline app",
+def main():
+  parser = argparse.ArgumentParser( prog="Commandline app",
                                   description="Counter interruption by space key")
 
-parser.add_argument('--target', type=int, required=True)
+  parser.add_argument('--target', type=int, required=True)
     
-args = parser.parse_args()
-target = args.target
+  args = parser.parse_args()
+  target = args.target
 
-for counter in range(1, target + 1):
+  for counter in range(1, target + 1):
         if keyboard.is_pressed('space'):
             print("Counter stopped by pressing space key.")
             break
 
         print(counter)
         time.sleep(1)
+
+
+if __name__ ==  '__main__':
+      main()
